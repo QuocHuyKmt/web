83104a603ad79f1b927c001c39c8163824d27f72
quochuykmt <quochuy765@gmail.com>
2018-04-16 22:23:48 +0700

    before

---
+++ checkout.php
@@ -0,0 +1,201 @@
+<!--A Design by W3layouts 
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Mattress A Ecommerce Category Flat Bootstrap Responsive Website Template | checkout :: w3layouts</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<!--theme-style-->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />	
+<!--//theme-style-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Mattress Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--fonts-->
+<link href='//fonts.googleapis.com/css?family=Lato:100,300,400,700,900' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'><!--//fonts-->
+<!-- start menu -->
+<link href="css/memenu.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/memenu.js"></script>
+<script>$(document).ready(function(){$(".memenu").memenu();});</script>
+<script src="js/simpleCart.min.js"> </script>
+</head>
+<body>
+<!--header-->
+        <?php include("./modules/header.php");?>
+	<!-- grow -->
+	<div class="grow">
+		<div class="container">
+			<h2>Checkout</h2>
+		</div>
+	</div>
+	<!-- grow -->
+<div class="container">
+	<div class="check">	 
+			 <h1>My Shopping Bag (2)</h1>
+		 <div class="col-md-9 cart-items">
+			
+				<script>$(document).ready(function(c) {
+					$('.close1').on('click', function(c){
+						$('.cart-header').fadeOut('slow', function(c){
+							$('.cart-header').remove();
+						});
+						});	  
+					});
+			   </script>
+			 <div class="cart-header">
+				 <div class="close1"> </div>
+				 <div class="cart-sec simpleCart_shelfItem">
+						<div class="cart-item cyc">
+							 <img src="images/pic1.jpg" class="img-responsive" alt=""/>
+						</div>
+					   <div class="cart-item-info">
+						<h3><a href="#">Mountain Hopper(XS R034)</a><span>Model No: 3578</span></h3>
+						<ul class="qty">
+							<li><p>Size : 5</p></li>
+							<li><p>Qty : 1</p></li>
+						</ul>
+						
+							 <div class="delivery">
+							 <p>Service Charges : Rs.100.00</p>
+							 <span>Delivered in 2-3 business days</span>
+							 <div class="clearfix"></div>
+				        </div>	
+					   </div>
+					   <div class="clearfix"></div>
+											
+				  </div>
+			 </div>
+			 <script>$(document).ready(function(c) {
+					$('.close2').on('click', function(c){
+							$('.cart-header2').fadeOut('slow', function(c){
+						$('.cart-header2').remove();
+					});
+					});	  
+					});
+			 </script>
+			 <div class="cart-header2">
+				 <div class="close2"> </div>
+				  <div class="cart-sec simpleCart_shelfItem">
+						<div class="cart-item cyc">
+							 <img src="images/pic2.jpg" class="img-responsive" alt=""/>
+						</div>
+					   <div class="cart-item-info">
+						<h3><a href="#">Mountain Hopper(XS R034)</a><span>Model No: 3578</span></h3>
+						<ul class="qty">
+							<li><p>Size : 5</p></li>
+							<li><p>Qty : 1</p></li>
+						</ul>
+							 <div class="delivery">
+							 <p>Service Charges : Rs.100.00</p>
+							 <span>Delivered in 2-3 business days</span>
+							 <div class="clearfix"></div>
+				        </div>	
+					   </div>
+					   <div class="clearfix"></div>
+											
+				  </div>
+			  </div>		
+		 </div>
+		  <div class="col-md-3 cart-total">
+			 <a class="continue" href="#">Continue to basket</a>
+			 <div class="price-details">
+				 <h3>Price Details</h3>
+				 <span>Total</span>
+				 <span class="total1">6200.00</span>
+				 <span>Discount</span>
+				 <span class="total1">---</span>
+				 <span>Delivery Charges</span>
+				 <span class="total1">150.00</span>
+				 <div class="clearfix"></div>				 
+			 </div>	
+			 <ul class="total_price">
+			   <li class="last_price"> <h4>TOTAL</h4></li>	
+			   <li class="last_price"><span>6350.00</span></li>
+			   <div class="clearfix"> </div>
+			 </ul>
+			
+			 
+			 <div class="clearfix"></div>
+			 <a class="order" href="#">Place Order</a>
+			 <div class="total-item">
+				 <h3>OPTIONS</h3>
+				 <h4>COUPONS</h4>
+				 <a class="cpns" href="#">Apply Coupons</a>
+				 <p><a href="#">Log In</a> to use accounts - linked coupons</p>
+			 </div>
+			</div>
+		
+			<div class="clearfix"> </div>
+	 </div>
+	 </div>
+
+
+<!--//content-->
+<div class="footer w3layouts">
+				<div class="container">
+			<div class="footer-top-at w3">
+			
+				<div class="col-md-3 amet-sed w3l">
+				<h4>MORE INFO</h4>
+				<ul class="nav-bottom">
+						<li><a href="#">How to order</a></li>
+						<li><a href="#">FAQ</a></li>
+						<li><a href="contact.php">Location</a></li>
+						<li><a href="#">Shipping</a></li>
+						<li><a href="#">Membership</a></li>	
+					</ul>	
+				</div>
+				<div class="col-md-3 amet-sed w3ls">
+					<h4>CATEGORIES</h4>
+					<ul class="nav-bottom">
+						<li><a href="#">Bed linen</a></li>
+						<li><a href="#">Cushions</a></li>
+						<li><a href="#">Duvets</a></li>
+						<li><a href="#">Pillows</a></li>
+						<li><a href="#">Protectors</a></li>	
+					</ul>
+					
+				</div>
+				<div class="col-md-3 amet-sed agileits">
+					<h4>NEWSLETTER</h4>
+					<div class="stay agileinfo">
+						<div class="stay-left wthree">
+							<form action="#" method="post">
+								<input type="text" placeholder="Enter your email " required="">
+							</form>
+						</div>
+						<div class="btn-1 w3-agileits">
+							<form action="#" method="post">
+								<input type="submit" value="Subscribe">
+							</form>
+						</div>
+							<div class="clearfix"> </div>
+			</div>
+					
+				</div>
+				<div class="col-md-3 amet-sed agileits-w3layouts">
+				<h4>CONTACT US</h4>
+					<p>Contrary to popular belief</p>
+					<p>The standard chunk</p>
+					<p>office :  +12 34 995 0792</p>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<div class="footer-class w3-agile">
+		<p>© 2015 Mattress . All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+		</div>
+		</div>
+</body>
+</html>
+			
---
+++ modules/header.php
@@ -0,0 +1,65 @@
+<div class="header">
+            <div class="header-top">
+                <div class="container">
+                    <div class="social">
+                        <ul>
+                            <li><a href="https://facebook.com/quochuykmt"><i class="facebok"> </i></a></li>
+                            <li><a href="#"><i class="inst"> </i></a></li>
+                            <div class="clearfix"></div>	
+                        </ul>
+                    </div>
+                    <div class="header-left">
+
+                        <div class="search-box">
+                            <div id="sb-search" class="sb-search">
+                                <form action="#" method="post">
+                                    <input class="sb-search-input" placeholder="Enter your search term..." type="search"  id="search">
+                                    <input class="sb-search-submit" type="submit" value="">
+                                    <span class="sb-icon-search"> </span>
+                                </form>
+                            </div>
+                        </div>
+
+                        <!-- search-scripts -->
+                        <script src="js/classie.js"></script>
+                        <script src="js/uisearch.js"></script>
+                        <script>
+            new UISearch(document.getElementById('sb-search'));
+                        </script>
+                        <!-- //search-scripts -->
+
+                        <div class="ca-r">
+                            <div class="cart box_1">
+                                <a href="checkout.php">
+                                    <h3> <div class="total">
+                                            <span class="simpleCart_total"></span> </div>
+                                        <img src="images/cart.png" alt=""/></h3>
+                                </a>
+                                <p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+
+                            </div>
+                        </div>
+                        <div class="clearfix"> </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="container">
+                <div class="head-top">
+                    <div class="logo">
+                        <h1><a href="index.php">Tái tạo da</a></h1>
+                    </div>
+                    <div class=" h_menu4">
+                        <ul class="memenu skyblue">
+                            <li><a class="color8" href="index.php">BED LINEN2</a></li>	
+                            <li><a class="color1" href="#">CUSHIONS</a></li>
+                            <li class="grid"><a class="color2" href="#">BEDSPREADS</a></li>
+                            <li><a class="color4" href="login.php">Login</a></li>				
+                            <li><a class="color6" href="contact.php">Contact</a></li>
+                        </ul> 
+                    </div>
+
+                    <div class="clearfix"> </div>
+                </div>
+            </div>
+        </div>
